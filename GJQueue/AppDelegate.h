80ca5411e51134aec36fe80574bb5a242ca0a4e4
tongguan <tongguan@guangjinminni.local>
2016-03-15 15:15:56 +0800

    Initial Commit

---
+++ GJQueue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GJQueue
+//
+//  Created by tongguan on 16/3/15.
+//  Copyright © 2016年 MinorUncle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GJQueue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GJQueue
+//
+//  Created by tongguan on 16/3/15.
+//  Copyright © 2016年 MinorUncle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
